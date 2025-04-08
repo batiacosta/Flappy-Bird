8f8d2a5bc468433e91c506ba2b2dbbb3b534b882
David Acosta <david.acosta.ing@gmail.com>
2025-04-08 09:55:56 -0500

    Creating pipes from top and bottom

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,19 +9,33 @@ public class Level : MonoBehaviour
     private const float CameraOrthoSize = 50f;
     private void Start()
     {
-        CreatePipe(50f, 0f);
-        CreatePipe(50f, 20f);
+        CreatePipe(50f, 0f, true);
+        CreatePipe(50f, 20f, false);
     }
 
-    private void CreatePipe(float height, float xPosition)
+    private void CreatePipe(float height, float xPosition, bool createBottom)
     {
+        var pipeHeadPositionY = 0f;
+        var pipeBodyPositionY = 0f;
         var pipeHead = Instantiate(GameAssets.GetInstance().GetPipeHead());
-        pipeHead.SetPositionAndRotation(new Vector2(xPosition, height - (PipeHeadHeight/2) - CameraOrthoSize), Quaternion.identity);
-        
         var pipeBody = Instantiate(GameAssets.GetInstance().GetPipeBody());
-        pipeBody.SetPositionAndRotation(new Vector2(xPosition, -CameraOrthoSize), Quaternion.identity);
+        if (createBottom)
+        {
+            pipeHeadPositionY = height - (PipeHeadHeight / 2) - CameraOrthoSize;
+            pipeBodyPositionY = -CameraOrthoSize;
+        }
+        else
+        {
+            pipeHeadPositionY = CameraOrthoSize - height + PipeHeadHeight / 2;
+            pipeBodyPositionY = CameraOrthoSize;
+            pipeBody.localScale = new Vector3(1, -1, 1); // So it grows inverted
+        }
+        
+        pipeHead.SetPositionAndRotation(new Vector2(xPosition, pipeHeadPositionY), Quaternion.identity);
+        
+        pipeBody.SetPositionAndRotation(new Vector2(xPosition, pipeBodyPositionY), Quaternion.identity);
         var pipeBodyRenderer = pipeBody.gameObject.GetComponent<SpriteRenderer>(); ;
-        pipeBodyRenderer.drawMode = SpriteDrawMode.Sliced; // Making sure Sliced is selected to being able to set size
+        pipeBodyRenderer.drawMode = SpriteDrawMode.Sliced; // Making sure Sliced is selected to be able to set size
         pipeBodyRenderer.size = new Vector2(PipeWidth, height);
         var pipeBodyBoxCollider = pipeBody.gameObject.GetComponent<BoxCollider2D>();
         pipeBodyBoxCollider.size = new Vector2(PipeWidth, height);
