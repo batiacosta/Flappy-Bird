27b1b220469efe4cc583e1d08614d93597fd4d06
David Acosta <david.acosta.ing@gmail.com>
2025-04-08 20:33:12 -0500

    Setting up basic restart after dying

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    public static Bird instance;
+    public Action OnDeath;
+    
     private Rigidbody2D _rigidbody;
     private const float JUMP_VALUE = 100f;
-    
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         if(_rigidbody == null) return;
         InputHandler.OnTap += OnTap;
     }
-
     private void OnDestroy()
     {
         InputHandler.OnTap -= OnTap;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _rigidbody.bodyType = RigidbodyType2D.Static;
+        OnDeath?.Invoke();
     }
 
     private void OnTap()
