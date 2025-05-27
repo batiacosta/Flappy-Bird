3a4a7f7416b34910a65e95505af284ddaf0cbf3f
David Acosta <david.acosta.ing@gmail.com>
2025-05-27 11:08:21 -0500

    Main Menu using UI Toolkit

---
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    private Button _playButton;
+    private Button _quitButton;
+    private void Start()
+    {
+        // Look for the buttons and add listeners
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        _playButton = root.Q<Button>("PlayButton");
+        _quitButton = root.Q<Button>("QuitButton");
+        // Set play button as default
+        _playButton.Focus();
+        _playButton.clicked += PlayButtonClicked;
+        _quitButton.clicked += QuitButtonClicked;
+    }
+
+    private void QuitButtonClicked()
+    {
+        SoundManager.PlaySound(SoundManager.Sound.ButtonClick);
+        Application.Quit();
+    }
+
+    private void PlayButtonClicked()
+    {
+        SoundManager.PlaySound(SoundManager.Sound.ButtonClick);
+        Loader.LoadScene(Loader.Scene.GameScene);
+    }
+
+    private void OnDestroy()
+    {
+        // All required unsubscriptions
+        _playButton.clicked -= PlayButtonClicked;
+        _quitButton.clicked -= QuitButtonClicked;
+    }
+}
